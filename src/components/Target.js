2d18d1f9cfc2b07aa78f5a78075d22ecf78116e8
Janhvi Singh <janhvi52@gmail.com>
2023-03-22 18:01:52 +0530

    images added

--- src/components/Target.js
+++ src/components/Target.js
@@ -12,9 +12,14 @@ function collect(connect, monitor) {
 class Target extends Component {
   render() {
     const {connectDropTarget, image} = this.props;
+    console.log(image);
+    const style1 ={width: "110%", height:"520px", overflow:"hidden"}
+    const style2= { width: "110%", height:"500px", overflow:"hidden" , marginTop: "20px"}
+    const style3= image.includes("6")?style1:style2;
+    console.log(style3);
     return connectDropTarget(
       <div className="target">
-        <img src={image} alt="glass" style={{ width: "110%", height:"500px", overflow:"hidden" }}></img>
+        <img src={image} alt="glass" style={style3}></img>
       </div>
     );
   }
